eec996052e3a8ed4732cea4ef12b753f02312dc1
Cory Todd <cory@ardonyx.io>
2020-04-25 11:11:50 -0700

    Add deployment script
    Not sure if this is going to work correctly, we'll see

---
+++ deploy.ps1
@@ -0,0 +1,15 @@
+param (
+    [string] $message = "site rebuild" + (Get-Date -UFormat "%A %m/%d/%Y %R %Z")
+)
+
+Write-Host "Deploying updates to Github"
+
+hugo -t hermit
+
+cd public
+
+git add .
+
+git commit -m $message
+
+git push origin master
